ec650c5e7cce2472ff02560fcd62b328db31a195
LSJiqueue <107478655+LSJiqueue@users.noreply.github.com>
2024-03-20 21:19:45 -0400

    Delete ButtonEffects.js

--- ButtonEffects.js
+++
@@ -1,9 +0,0 @@
-const startbutton = document.getElementById("start");
-function startButtonHovered(){
-    startbutton.style.backgroundColor = "green";
-};
-function startButtonLeft(){
-    startbutton.style.backgroundColor = "lightblue";
-};
-startbutton.addEventListener('mouseenter', startButtonHovered);
-startbutton.addEventListener('mouseleave', startButtonLeft);
